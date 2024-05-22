c92b9a4049cefc043dfc78d5af4e01918702771d
VallesBrian <21045148@alumno.utc.edu.mx>
2024-05-22 11:53:11 -0600

    Se realizo el front de login y se empezara a trabajar con su logica, para lo que se creo una clase usuario que controle estas acciones

---
+++ login.php
@@ -0,0 +1,164 @@
+
+<?php 
+session_start();
+
+
+if (isset($_POST['login'])) {
+    require 'bd_conexion.php';
+
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+
+
+    $query=$cnnPDO->prepare('SELECT * from clientes WHERE email=:email and password=:password');
+
+    $query->bindParam(':email',$email);
+    $query->bindParam(':password',$password);
+
+
+    $query->execute();
+
+    $count=$query->rowCount();
+    $campo = $query->fetch();
+    
+    if ($count) 
+    {
+    $_SESSION['nombre'] = $campo['nombre'];
+    $_SESSION['apellidos'] = $campo['apellidos'];
+    $_SESSION['email'] = $campo['email'];  
+    $_SESSION ['password'] =$campo['password'];
+    $_SESSION['rfc'] = $campo['rfc'];  
+    $_SESSION ['direccion'] =$campo['direccion'];
+
+
+    header("location:inicio.php");
+        
+    }
+    
+        
+}
+ob_end_flush();
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+		<title>Electro - HTML Ecommerce Template</title>
+
+ 		<!-- Google font -->
+ 		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
+
+ 		<!-- Bootstrap -->
+ 		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+
+ 		<!-- Slick -->
+ 		<link type="text/css" rel="stylesheet" href="css/slick.css"/>
+ 		<link type="text/css" rel="stylesheet" href="css/slick-theme.css"/>
+
+ 		<!-- nouislider -->
+ 		<link type="text/css" rel="stylesheet" href="css/nouislider.min.css"/>
+
+ 		<!-- Font Awesome Icon -->
+ 		<link rel="stylesheet" href="css/font-awesome.min.css">
+
+ 		<!-- Custom stlylesheet -->
+ 		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+
+ 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+ 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+ 		<!--[if lt IE 9]>
+ 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+ 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+ 		<![endif]-->
+
+    </head>
+	<body>
+		<!-- HEADER AND NAVBAR -->
+		<?php include_once "./templates/headNav.php"; ?>
+
+		<!-- BREADCRUMB -->
+		<div id="breadcrumb" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+						<h3 class="breadcrumb-header">My Account</h3>
+						<ul class="breadcrumb-tree">
+							<li><a href="./">Home</a></li>
+							<li class="active">Login</li>
+						</ul>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /BREADCRUMB -->
+
+		<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+                    <!-- SECTION -->
+                    <div class="section">
+                        <!-- container -->
+                        <div class="container">
+                            <!-- row -->
+                            <div class="row">
+                                <div class="col-md-7">
+                                    <img style="width: 100%; height: 50%;" class="img-fluid" src="https://ecelectronics.com/wp-content/uploads/2020/04/Modern-Electronics-EC-.jpg" alt="">    
+                                </div>
+
+                                <!-- Order Details -->
+                                <div class="col-md-5 order-details">
+                                    <div class="section-title text-center">
+                                        <h3 class="title">Login to your Account</h3>
+                                    </div>
+                                    <form method="post">
+                                        <div style="margin-bottom: 1em;">
+                                            <div><strong>Email</strong></div>
+                                            <div><input type="email" name="email" placeholder="Email" class="form-control" style="margin-bottom: 1em;"></div>
+                                            
+                                            <div><strong>Password</strong></div>
+                                            <div><input type="password" name="password" placeholder="Password" class="form-control"></div>
+                                        </div>
+                                        <div>
+                                            <p>You don't have an account? <a href="./register.php">Create one</a></p>
+                                        </div>
+                                        <button type="submit" name="login" class="primary-btn order-submit" style="width: 100%;">Login</button>
+                                    </form>
+                                </div>
+                                <!-- /Order Details -->
+                            </div>
+                            <!-- /row -->
+                        </div>
+                        <!-- /container -->
+                    </div>
+                    <!-- /SECTION -->
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+
+		<!-- NEWSLETER AND FOOTER -->
+		<?php include_once "./templates/newlFoot.php"; ?>
+
+		<!-- jQuery Plugins -->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/slick.min.js"></script>
+		<script src="js/nouislider.min.js"></script>
+		<script src="js/jquery.zoom.min.js"></script>
+		<script src="js/main.js"></script>
+
+	</body>
+</html>
--- myAccount.php
+++ myAccount.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['id_user'])) {
+    header('Location: login.php');
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
